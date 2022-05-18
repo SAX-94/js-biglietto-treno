1100c854f495dc7bfae8ca6b1db84944031c24b7
SAX_94 <matteosacchi94@gmail.com>
2022-05-18 23:10:15 +0200

    finished mandatory tasks

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// VARIABILI
+const pricePerkm = 0.21;
+const kidsDiscount = 0.8;
+const elderDiscount = 0.6;
+let discount;
+
+let age = parseInt(prompt("Quanti anni hai?"));
+let km = parseInt(prompt("Quanti km devi percorrere?"));
+let price = parseFloat(pricePerkm * km);
+
+
+if (age < 18) {
+    discount = kidsDiscount;
+} else if (age > 65) {
+    discount = elderDiscount;
+} else {
+    discount = 1;
+}
+
+let finalPrice = parseFloat(price * discount).toFixed(2);
+
+// VALORI DA TRASFERIRE SU HTML
+document.getElementById("age").innerHTML = age;
+document.getElementById("km").innerHTML = km;
+document.getElementById("price").innerHTML = price;
+document.getElementById("finalprice").innerHTML = finalPrice;
